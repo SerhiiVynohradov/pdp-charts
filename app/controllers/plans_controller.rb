a641e11d546c1926d1654cf262cb766bc3c9b4db
Serge Vinogradoff <sergey.vynogradov@gotoinc.co>
2025-02-03 00:05:29 +0200

    add a todo

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -8,6 +8,28 @@ class PlansController < ApplicationController
 
   def update_role
     plan = params[:plan]
+
+    # TODO:
+    # Case 1: User becomes Manager
+    # - is a manager of his own team
+    # - still uses the other team as a user
+    # Case 2: Manager becomes Owner
+    # - creates his own company
+    # - still manages the other company's team
+    # Case 3: User becomes Owner
+    # - creates his own company
+    # - still has his own items (without a team)
+    # Case 4: Manager becomes User
+    # - no longer is a manager of his team? Orphan team?
+    # Case 5: Owner becomes User
+    # - no longer has a company? Orphan company?
+    # Case 6: Owner becomes Manager
+    # - no longer has a company? Orphan company?
+    # - is a manager of his own team
+    #
+    # This is at least HABTM + pay per company/team created.
+    # It's not a simple role change.
+
     if current_user.update(role: plan)
       if current_user.manager? && current_user.team.blank?
         redirect_to new_team_path
