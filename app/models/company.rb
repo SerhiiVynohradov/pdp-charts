b991a7d1da65ffe786548188018a4c7dde83a327
Serge Vinogradoff <sergey.vynogradov@gotoinc.co>
2025-02-08 20:39:12 +0200

    fix bug

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,7 @@ class Company < ApplicationRecord
 
   has_many :teams, dependent: :destroy
   has_many :users, dependent: :nullify
-  has_one :owner, class_name: 'User', foreign_key: 'company_owner_id', dependent: :nullify
+  has_one :owner, class_name: 'User', foreign_key: 'company_id', dependent: :nullify
   has_many :invoices, as: :invoiceable, dependent: :nullify
 
   has_many :events, as: :eventable, dependent: :destroy
