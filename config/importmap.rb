110e93b0e4f0d68c635cd0373865c3caa2679aad
Serge Vinogradoff <sergey.vynogradov@gotoinc.co>
2025-03-15 12:15:50 +0200

    alpinejs from a stable source

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "tailwind-scrollbar" # @4.0.1
 pin "tailwindcss/lib/util/flattenColorPalette", to: "tailwindcss--lib--util--flattenColorPalette.js" # @4.0.9
 pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.0.9
+pin "alpinejs", to: "alpinejs.min.js"
